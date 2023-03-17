206fb46f925c9fe079014cf03c11d008a3e3a23a
senxu.tian <191341025@qq.com>
2023-03-17 16:55:04 +0800

    learn python 01 day

---
+++ Day01-15/code/Day01/hello.py
@@ -0,0 +1,22 @@
+"""
+第一个Python程序 - hello, world
+向伟大的Dennis M. Ritchie先生致敬
+
+Version: 0.1
+Author:
+Date: 2023-03-17
+
+请将改文件命名为hello.py
+
+使用windows的小伙伴可以在命令行提示下通过下面的命令运行该程序
+python hello.py
+
+对于使用Linux或者macOS的小伙伴可以打开终端并键入下面的命令来运行程序
+python3 hello.py
+"""
+
+print("hello, world!")
+# print("你好，世界！")
+print("你好，世界！")
+print('hello', 'world', sep=', ', end='!\n')
+print('goodbye, world', end='!\n')
